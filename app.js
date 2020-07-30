738ff649e41d1ac21a2607bfd8480062a0908bb8
MohinishTeja <45705872+MohinishTeja@users.noreply.github.com>
2020-07-30 18:44:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ VANTA.WAVES({
   minHeight: 200.00,
   minWidth: 200.00,
   scale: 1.00,
-  scaleMobile: 1.00
+  scaleMobile: 1.00,
+  color: 0x8886,
+  shininess: 90.00,
+  waveHeight: 18.50,
+  waveSpeed: 1.25,
+  zoom: 0.65
 })
 
 
